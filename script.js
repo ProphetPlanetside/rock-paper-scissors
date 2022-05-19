d8ed0c6f3fd0120b560377f61413fabd1fb3f27f
Jay Spendlove <jayaloha@hotmail.com>
2022-05-19 12:12:00 -0700

    Created function playRound

--- script.js
+++ script.js
@@ -17,4 +17,38 @@ function computerPlay() {
     }
 }
 
-console.log(computerPlay());
+// Take a string 'playerSelection' and convert it to all lowercase.
+// Compare playerSelection to computerSelection and see who wins.
+// Return a message saying 'you lose', 'you tie', or 'you win.'
+function playRound(playerSelection, computerSelection) {
+    // Convert playerSelection string to be all lowercase.
+    playerSelection = playerSelection.toLowerCase();
+    // The player and computer tie.
+    if (playerSelection == computerSelection) {
+        return 'A Tie! ' + playerSelection + ' ties with ' + computerSelection;
+    }
+    else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return 'You Lose! ' + computerSelection + ' beats ' + playerSelection;
+    }
+    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return 'You Win! ' + playerSelection + ' beats ' + computerSelection;
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return 'You Lose! ' + computerSelection + ' beats ' + playerSelection;
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return 'You Win! ' + playerSelection + ' beats ' + computerSelection;
+    }
+    // Implied that playerSelection == 'scissors'
+    else if (computerSelection == 'rock') {
+        return 'You Lose! ' + computerSelection + ' beats ' + playerSelection;
+    }
+    // Implied that playerSelection == 'scissors'
+    else {
+        return 'You Win! ' + playerSelection + ' beats ' + computerSelection;
+    }
+}
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
